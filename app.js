e26d3c10d0d8025523936ae9c8bfe3d7387c13ab
debowski <roae@poczta.fm>
2022-12-09 00:00:43 +0100

    Menu na stronę
    Dodane menu

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-var data = JSON.parse(xhr.responseText);
+const menuToggle = document.querySelector('.menu-toggle');
+const siteNavigation = document.querySelector('.primary-navigation');
 
-console.log(data.content);
+menuToggle.addEventListener('click', () => {
+  const isOpened = menuToggle.getAttribute('aria-expanded') === "true";
+  if (isOpened ? closeMenu() : openMenu());
+});
+
+function openMenu() {
+  menuToggle.setAttribute('aria-expanded', "true");
+  siteNavigation.setAttribute('data-state', "opened");
+}
+function closeMenu() {
+  menuToggle.setAttribute('aria-expanded', "false");
+  siteNavigation.setAttribute('data-state', "closing");
+
+  siteNavigation.addEventListener('animationend', () => {
+    siteNavigation.setAttribute('data-state', "closed");
+  }, {once: true})
+}
